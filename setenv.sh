ca6134e91751d7b739bfee96f8a39c91cf69c92f
Tim Caswell <tim@creationix.com>
2016-05-05 15:52:41 -0500

    Fix internal paths in luarocks

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,6 @@
-LUA=$(pwd)/$LUA
-LUVIT=($pwd)/luvit/2.10.1
+export LENV=$(pwd) # Should be this directory
+LUA=$LENV/$LUA
+LUVIT=$LENV/luvit/2.10.1
 if [[ $LUA == luajit* ]]; then VER=5.1; fi
 if [[ $LUA == lua/5.1* ]]; then VER=5.1; fi
 if [[ $LUA == lua/5.2* ]]; then VER=5.2; fi
@@ -7,4 +8,3 @@ if [[ $LUA == lua/5.3* ]]; then VER=5.3; fi
 export PATH=$LUVIT:$LUA/bin:$PATH
 export LUA_PATH="./?.lua;$LUA/share/lua/$VER/?.lua;$LUA/share/lua/$VER/?/init.lua;;"
 export LUA_CPATH="./?.so;$LUA/lib/lua/$VER/?.so;$LUA/share/lua/$VER/?.so;;"
-
