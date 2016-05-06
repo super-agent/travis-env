7658129a2b71b6ca0cc2142404f6526a734e4584
Tim Caswell <tim@creationix.com>
2016-05-06 13:14:51 -0500

    Add luvit bin as fallback path to get lit command

--- setenv.sh
+++ setenv.sh
@@ -4,13 +4,13 @@ if [ -n "$BASH_SOURCE" ]; then
 fi
 export LENV="$(cd "$(dirname "${SCRIPT_SOURCE:-$0}")" > /dev/null && pwd)/$(uname -s)-$(uname -m)"
 PREFIX=$LENV/$LUA
-export PATH=$PREFIX/bin:$PATH
+export PATH=$PREFIX/bin:$LENV/luvit-2/bin:$PATH
 if [[ "$LUA" != luvit-* ]]; then
   if [[ "$LUA" == luajit-* ]]; then VER=5.1; fi
   if [[ "$LUA" == lua-5.1* ]]; then VER=5.1; fi
   if [[ "$LUA" == lua-5.2* ]]; then VER=5.2; fi
   if [[ "$LUA" == lua-5.3* ]]; then VER=5.3; fi
   LUAROOT=$LENV/$LUA
-  export LUA_PATH="./?.lua;$LUAROOT/share/lua/$VER/?.lua;$LUAROOT/share/lua/$VER/?/init.lua;;"
-  export LUA_CPATH="./?.so;$LUAROOT/lib/lua/$VER/?.so;$LUAROOT/share/lua/$VER/?.so;;"
+  export LUA_PATH="./?.lua;$LUAROOT/share/lua/$VER/?.lua;$LUAROOT/share/lua/$VER/?/init.lua;"
+  export LUA_CPATH="./?.so;$LUAROOT/lib/lua/$VER/?.so;$LUAROOT/share/lua/$VER/?.so;"
 fi
